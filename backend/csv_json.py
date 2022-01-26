d707cd428b161e80a2bcd507262fc6afefe21800
Vidushi Gupta <55969597+Vidushi-Gupta@users.noreply.github.com>
2022-01-26 23:06:46 +0530

    Separating node wise end points

--- backend/csv_json.py
+++ backend/csv_json.py
@@ -2,10 +2,10 @@ from flask import Flask, request
 import json
 import csv
 import pandas as pd
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app, support_credentials=True)
+# CORS(app, support_credentials=True)
 
 df_1 = pd.read_csv("Sensor Data.csv")
 df_1 = df_1[df_1['Node ID'] == 1]
@@ -15,9 +15,11 @@ df_2 = pd.read_csv("Sensor Data.csv")
 df_2 = df_2[df_2['Node ID'] == 2]
 df_2.to_csv('Sensor Data 2.csv')
 
+# Sensor data for node 1
 
-@app.route('/sensor_data', methods=['GET', 'POST'])
-def sensor_data():
+
+@app.route('/sensor_data1', methods=['GET', 'POST'])
+def sensor_data1():
     with open("Sensor Data 1.csv", "r") as f:
         reader = csv.reader(f)
         next(reader)
@@ -31,13 +33,19 @@ def sensor_data():
                 "Light": row[8],
                 "Sound": row[9]
             })
-    with open("sensor_data.json", "w") as f:
+    with open("sensor_data1.json", "w") as f:
         json.dump(data, f)
-    f = open("sensor_data.json", "r")
+    f = open("sensor_data1.json", "r")
     file_contents = f.read()
+    return (file_contents)
 
-    with open("Sensor Data 2.csv", "r") as f1:
-        reader = csv.reader(f1)
+# Sensor data for node 2
+
+
+@app.route('/sensor_data2', methods=['GET', 'POST'])
+def sensor_data2():
+    with open("Sensor Data 2.csv", "r") as f:
+        reader = csv.reader(f)
         next(reader)
         data = []
 
@@ -49,12 +57,11 @@ def sensor_data():
                 "Light": row[8],
                 "Sound": row[9]
             })
-    with open("sensor_data.json", "w") as f1:
-        json.dump(data, f1)
-    f1 = open("sensor_data.json", "r")
-    file1_contents = f1.read()
-
-    return 'Node 1:{} , Node 2:{}'.format(file_contents, file1_contents)
+    with open("sensor_data2.json", "w") as f:
+        json.dump(data, f)
+    f = open("sensor_data2.json", "r")
+    file_contents = f.read()
+    return (file_contents)
 
 
 @app.route('/total_traffic', methods=['GET', 'POST'])
